a8fd2556b7098ea255de71cb1c706d0a6f82d165
croueAlec <alec.croue.pro@gmail.com>
2026-06-11 10:51:55 +0200

    feat : print password on binary extraction

--- tools/extract_binary.sh
+++ tools/extract_binary.sh
@@ -1,20 +1,49 @@
 #! /bin/bash
 
+function print_extraction() {
+	bold_value="\033[1m$1\033[0m"
+	echo -e "Extracting ./$bold_value from user $bold_value"
+	echo -n "Password is : "
+}
+
+function print_bold() {
+	echo -e "\033[1m$1\033[0m"
+}
+
+function print_flag() {
+	previous_user="$1"
+
+	if [ ! -s "./$previous_user/flag" ]; then
+		echo "Error : Flag has not been found yet, go back to work."
+		exit 1
+	fi
+	print_extraction $VM_USER; 
+	print_bold $(cat ./$previous_user/flag)
+}
+
 if [ -z "$1" ]; then
-	echo "Error : No user binary specified";
+	echo "Error : No user specified";
 	exit 1
 else
 	VM_USER="$1"
 	if [[ "$VM_USER" =~ ^(level[0-9]|bonus[0-3])$ ]]; then
-		echo "Extracting $VM_USER"
 
-		mkdir -p binaries/
-		scp -P 4242 $VM_USER@localhost:/home/user/$VM_USER/$VM_USER ./binaries/.
+		num=${VM_USER##*[!0-9]}
+		prefix=${VM_USER%"$num"}
 
-		exit 0
+		previous_user="${prefix}$((num - 1))"
+
+		case "$VM_USER" in
+		"level0") print_extraction $VM_USER; print_bold "level0"
+		;;
+		"bonus0") print_flag "level9"
+		;;
+		*) print_flag "$previous_user"
+		esac
+
+		scp -P 4242 $VM_USER@localhost:/home/user/$VM_USER/$VM_USER ./binaries/. 2> /dev/null
 	else
-		echo "Error : Invalid user binary"
+		echo "Error : Invalid user name"
 		exit 1
 	fi
 fi
-
