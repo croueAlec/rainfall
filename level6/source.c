d2d9a1510cdbe6c7107775da5823b57405b08218
croueAlec <alec.croue.pro@gmail.com>
2026-06-15 15:41:03 +0200

    feat : add level6 source

---
+++ level6/source.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef void undefined4;
+
+/* Decompiled using Ghidra */
+
+
+void main(undefined4 param_1,int param_2)
+{
+  char *__dest;
+  undefined4 *puVar1;
+  
+  __dest = malloc(0x40);
+  puVar1 = malloc(4);
+  *puVar1 = m;
+  strcpy(__dest,*(char **)(param_2 + 4));
+  (*(code *)*puVar1)();
+  return;
+}
+
+void m(void *param_1,int param_2,char *param_3,int param_4,int param_5)
+{
+  puts("Nope");
+  return;
+}
+
+void n(void)
+{
+  system("/bin/cat /home/user/level7/.pass");
+  return;
+}
+
+void frame_dummy(void)
+{
+  return;
+}
