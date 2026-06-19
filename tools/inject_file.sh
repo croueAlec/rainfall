62dce3069e6d676998936889f9f101f1637b533f
croueAlec <alec.croue.pro@gmail.com>
2026-06-19 18:27:28 +0200

    feat : wip inject file script

---
+++ tools/inject_file.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+HOSTNAME="127.0.0.1"
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+	echo "Missing argument: Usage: ./inject_file.sh [target user] [password file path] [file]"
+	exit 1
+fi
+
+VM_USER="$1"
+PASS_PATH="./$2/flag"
+
+FILE="$3"
+
+echo "$FILE $(basename $FILE)"
+
+sshpass -f $PASS_PATH scp -P 4242 $FILE $VM_USER@$HOSTNAME:/tmp/$(basename $FILE) 2> /dev/null
+
+echo "Successfully copied $(basename $FILE) to /tmp/$(basename $FILE)"
